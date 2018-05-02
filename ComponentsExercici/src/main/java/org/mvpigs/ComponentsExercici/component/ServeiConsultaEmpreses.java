f8cd0517190a7145040e5a9920ccc2a4123f21ce
Unknown <andrivgar@gmail.com>
2018-05-03 00:44:07 +0200

    Second Test

--- ComponentsExercici/src/main/java/org/mvpigs/ComponentsExercici/component/ServeiConsultaEmpreses.java
+++ ComponentsExercici/src/main/java/org/mvpigs/ComponentsExercici/component/ServeiConsultaEmpreses.java
@@ -3,9 +3,12 @@ package org.mvpigs.ComponentsExercici.component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 // aquesta classe ha de ser detectada com un component
 @Component
 public class ServeiConsultaEmpreses {
+
 
     // aquest servei necessita d'un integrador de cotitzacions per retornar la informacio
     // declarau la dependencia i feis que spring la resolgui
@@ -15,8 +18,9 @@ public class ServeiConsultaEmpreses {
     // modifiqueu aquest metode per canviar el 0 per el valor obtingut del clientCotitzacionsWS
     public String infoEmpresa(String empresa) {
 
-        return "La empresa " + empresa + " cotitza a 0";
+        return "La empresa " + empresa + " cotitza a " + clientCotizacions.obteCotitzacio(empresa);
     }
+
 
 
     // modifiqueu aquest metode per canviar el 0 per el valor obtingut del clientCotitzacionsWS
