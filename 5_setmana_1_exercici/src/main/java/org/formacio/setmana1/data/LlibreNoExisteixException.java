670c7a37ec4cf2cac8af77d0bab5242cf4c8760a
Unknown <andrivgar@gmail.com>
2018-06-13 16:51:27 +0200

    Test 1

--- 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/data/LlibreNoExisteixException.java
+++
@@ -1,3 +0,0 @@
-package org.formacio.setmana1.data;
-
-public class LlibreNoExisteixException extends Exception {}
--- 5_setmana_1_exercici/src/main/java/org/formacio/setmana1/domini/Recomanacio.java
+++
@@ -1,5 +0,0 @@
-package org.formacio.setmana1.domini;
-
-public enum Recomanacio {
-   PRESCINDIBLE, RECOMANAT, OBRA_MESTRE;
-}
